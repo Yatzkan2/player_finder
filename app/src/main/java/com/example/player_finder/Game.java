37ba04ee7a1d95444aba7d331f4b8f965f417532
Yatzkan <yyatzkan@gmail.com>
2024-10-22 11:09:45 +0300

    games fragments using DB collection now

--- app/src/main/java/com/example/player_finder/Game.java
+++ app/src/main/java/com/example/player_finder/Game.java
@@ -3,8 +3,13 @@ package com.example.player_finder;
 import java.util.UUID;
 
 public class Game {
-    private final String id;
-    private final String title;
+    private String id;
+    private String title;
+
+    // No-argument constructor required for Firestore deserialization
+    public Game() {
+        //this.id = UUID.randomUUID().toString();  // Generate a new ID if needed
+    }
 
     public Game(String title) {
         this.id = UUID.randomUUID().toString();  // Generate a new ID if needed
